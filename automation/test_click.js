482fcbe5aa3839e2a0f51f567fba288f110ed34b
lejara <leoneljara3@gmail.com>
2025-07-02 01:54:43 -0400

    Add insert text test code

--- automation/test_click.js
+++ automation/test_click.js
@@ -1,29 +1,41 @@
 const { chromium } = require("playwright");
 
-let page = null; // Global page variable to be used in runAutomation
+let page = null;
+let browser = null;
 
 async function init() {
-  const browser = await chromium.launch({ headless: false }); // show browser
+  const userDataDir =
+    "C:/Users/Lejara/AppData/Local/Google/Chrome/User Data/Default"; //TODO: make this work for anyone
+  browser = await chromium.launchPersistentContext(userDataDir, {
+    headless: false,
+    viewport: null,
+    userAgent:
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+    args: [
+      "--disable-blink-features=AutomationControlled",
+      "--no-sandbox",
+      "--disable-web-security",
+      "--disable-features=IsolateOrigins,site-per-process",
+      "--disable-site-isolation-trials",
+      "--disable-extensions",
+      "--start-maximized",
+    ],
+  });
+
   page = await browser.newPage();
-  await page.goto("https://example.com");
+  await page.goto(
+    "https://jitter.video/file/?id=IfcoVaKzO4tQsGiLTECTcIbl&nodeId=Rx2ydPZha60gZUMr_W-KW"
+  );
 }
 
-async function runAutomation() {
-  await page.goto("https://www.youtube.com/");
+async function runAutomation() {}
 
-  // // Click a button
-  // await page.click("button#submit");
-
-  // // Fill a text input
-  // await page.fill('input[name="username"]', "myUser");
-
-  // // Modify DOM element directly
-  // await page.evaluate(() => {
-  //   const el = document.querySelector("#title");
-  //   if (el) el.textContent = "Changed Title!";
-  // });
-
-  // await browser.close();
+async function changeText() {
+  const textButton = page.getByText("#1-View the Journal page:");
+  await textButton.click();
+  await textButton.press("Enter");
+  await page.keyboard.press("Backspace");
+  await page.keyboard.insertText("Hello, world!");
 }
 
 module.exports = { runAutomation, init };
