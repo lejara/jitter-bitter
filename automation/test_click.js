35a30a8efa342b7c01def46ff84bd564ded3d542
lejara <leoneljara3@gmail.com>
2025-07-03 19:02:23 -0400

    Add text check

--- automation/test_click.js
+++ automation/test_click.js
@@ -1,4 +1,5 @@
 const { chromium } = require("playwright");
+const { text } = require("stream/consumers");
 
 let page = null;
 let browser = null;
@@ -23,7 +24,7 @@ async function init() {
 
   page = await browser.newPage();
   await page.goto(
-    "https://jitter.video/file/?id=3X4wwy1fOGPZdCZEtOtT5fKS&nodeId=P94KuG7e_o1XPNxmk5ueQ"
+    "https://jitter.video/file/?id=5TO8y3oziHnvWxBVLGWwnvvr&nodeId=GhOVX31xagbnTKErKfY3x"
   );
 }
 
@@ -32,11 +33,15 @@ async function init() {
 const locators = {
   parentSelectedClass: "layerList-module--selected--54ce4",
   childLayerListClass: "layerList-module--parentInSelection",
+  scrollContainerClass: 'data-sentry-element="EditorPanel"',
+  textSVG: 'path[d^="M5 6V4H19V8H17V6H13V18H15V20H13H11H9V"]',
 };
 const enterCount = 100;
 const parentSelectedID = "parent-frame-333";
-
 async function runAutomation() {
+  await scan();
+}
+async function scan() {
   //Get selected Element
   let selectedElement = await page
     .locator(`.${locators.parentSelectedClass}`)
@@ -67,7 +72,7 @@ async function runAutomation() {
   //Scan childern elemets
   let ctr = startingIndex + 1;
   while (true) {
-    let element = page.locator(`[data-index="${ctr}"] > *:first-child`, {
+    let element = await page.locator(`[data-index="${ctr}"] > *:first-child`, {
       hasText: undefined,
     });
 
@@ -82,24 +87,60 @@ async function runAutomation() {
       break;
     }
 
+    //Check if is text
+    const textSVG = await element.locator(locators.textSVG);
+
+    if ((await textSVG.count()) > 0) {
+      console.log("Text SVG found");
+      await changeText(element);
+    }
     // await element.click();
 
     // const className = await element.getAttribute("class");
     // console.log(
     //   `Element ${ctr} class: ${className} inline-start: ${computedPaddingStart}`
     // );
-    // console.log(ctr);
+    console.log(ctr);
     ctr++;
   }
+  console.log(`Scan Done: ${ctr}`);
 
-  // const count = await elements.count();
-  // for (let i = 0; i < count; i++) {
-  //   const element = elements.nth(i);
+  // const result = await page.evaluate(
+  //   ({ start, threshold, locators }) => {
+  //     let ctr = start + 1;
 
-  //   await element.evaluate((el) =>
-  //     el.scrollIntoView({ behavior: "instant", block: "nearest" })
-  //   );
-  // }
+  //     const scrollConatiner = document.querySelector(
+  //       `[${locators.scrollContainerClass}]`
+  //     );
+  //     while (true) {
+  //       const container = document.querySelector(`[data-index="${ctr}"]`);
+  //       if (!container) break;
+
+  //       const element = container.firstElementChild;
+  //       if (!element) break;
+
+  //       // element.scrollIntoView({ behavior: "instant", block: "center" });
+  //       scrollConatiner.scrollTop += 2;
+
+  //       const paddingStr = getComputedStyle(element).getPropertyValue(
+  //         "padding-inline-start"
+  //       );
+  //       const padding = parseFloat(paddingStr) || 0;
+
+  //       if (padding <= threshold) break;
+
+  //       ctr++;
+  //       console.log(ctr);
+  //     }
+  //     console.log(`Final count: ${ctr}`);
+  //     return ctr;
+  //   },
+  //   {
+  //     start: startingIndex,
+  //     threshold: selectedElementPadding,
+  //     locators: locators,
+  //   }
+  // );
 }
 
 async function GetPaddingInlineStart(locator) {
@@ -113,8 +154,7 @@ async function printDOM(locator) {
   console.log(domContent);
 }
 
-async function changeText() {
-  const textButton = page.getByText("#1-View the Journal page:");
+async function changeText(textButton) {
   await textButton.click();
   await textButton.press("Enter");
   await page.keyboard.press("Backspace");
