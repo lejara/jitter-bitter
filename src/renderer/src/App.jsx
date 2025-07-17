47e42ba5ca66906ff64fdb7396f4eca4c2c26dcd
lejara <leoneljara3@gmail.com>
2025-07-17 13:13:46 -0400

    Add tailwind

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import "../index.css";
 
 export default function App() {
   return (
     <div style={{ padding: "1rem", fontFamily: "sans-serif" }}>
-      <h1>Jitter Bitter</h1>
+      <h1 className="bg-red-100">Jitter Bitter</h1>
       <img src="/hackor2.png" alt="Logo from public folder" />
       <button
+        className="bg-red-100"
         onClick={async () => {
           // status.textContent = "Status: Running Scan Replace...";
           await window.electronAPI.runScanReplaceScript();
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   root: "src/renderer", // where index.html lives
   publicDir: "public",
-  plugins: [react()],
+  base: "./",
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: "../../dist/renderer", // build into dist/renderer
     emptyOutDir: true,
